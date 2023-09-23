e6afdc40c1f5d51b0a130abf1064a9544549590c
matt3o <matt3o@gmail.com>
2023-09-23 20:45:16 +0200

    added contrast adaptive sharpening

--- essentials.py
+++ essentials.py
@@ -283,7 +283,7 @@ class MaskPreview(SaveImage):
 
 class GrowShrinkMask:
     @classmethod
-    def INPUT_TYPES(cls):
+    def INPUT_TYPES(s):
         return {
             "required": {
                 "mask": ("MASK",),
@@ -312,6 +312,74 @@ class GrowShrinkMask:
             output = grey_action(output, footprint=kernel)
             amount -= 1
         output = torch.from_numpy(output)
+
+        return (output,)
+
+def min_(tensor_list):
+    # return the element-wise min of the tensor list.
+    x = torch.stack(tensor_list)
+    mn = x.min(axis=0)[0]
+    return mn
+    
+def max_(tensor_list):
+    # return the element-wise max of the tensor list.
+    x = torch.stack(tensor_list)
+    mx = x.max(axis=0)[0]
+    return mx
+
+class ImageCAS:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "amount": ("FLOAT", {"default": 0.8, "min": 0, "max": 1, "step": 0.05}),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    CATEGORY = "essentials"
+    FUNCTION = "execute"
+
+    def execute(self, image, amount):
+        img = F.pad(p(image), pad=(1, 1, 1, 1)).cpu()
+
+        a = img[..., :-2, :-2]
+        b = img[..., :-2, 1:-1]
+        c = img[..., :-2, 2:]
+        d = img[..., 1:-1, :-2]
+        e = img[..., 1:-1, 1:-1]
+        f = img[..., 1:-1, 2:]
+        g = img[..., 2:, :-2]
+        h = img[..., 2:, 1:-1]
+        i = img[..., 2:, 2:]
+        
+        # Computing contrast
+        cross = (b, d, e, f, h)
+        mn = min_(cross)
+        mx = max_(cross)
+        
+        diag = (a, c, g, i)
+        mn2 = min_(diag)
+        mx2 = max_(diag)    
+        mx = mx + mx2
+        mn = mn + mn2
+        
+        # Computing local weight
+        inv_mx = torch.reciprocal(mx) # 1/mx
+        
+        amp = inv_mx * torch.minimum(mn, (2 - mx))
+    
+        # scaling
+        amp = torch.sqrt(amp)
+        w = - amp * (amount * (1/5 - 1/8) + 1/8)
+        div = torch.reciprocal(1 + 4*w)
+
+        output = ((b + d + f + h)*w + e) * div
+        output = output.clamp(0, 1)
+        output = torch.nan_to_num(output)   # what am I doing?!
+
+        output = pb(output) 
 
         return (output,)
 
@@ -393,6 +461,7 @@ NODE_CLASS_MAPPINGS = {
 
     "ImageDesaturate+": ImageDesaturate,
     "ImagePosterize+": ImagePosterize,
+    "ImageCASharpening+": ImageCAS,
 
     "MaskBlur+": MaskBlur,
     "MaskFlip+": MaskFlip,
@@ -412,6 +481,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
 
     "ImageDesaturate+": "🔧 Image Desaturate",
     "ImagePosterize+": "🔧 Image Posterize",
+    "ImageCASharpening+": "🔧 Image Contrast Adaptive Sharpening",
 
     "MaskBlur+": "🔧 Mask Blur",
     "MaskFlip+": "🔧 Mask Flip",
