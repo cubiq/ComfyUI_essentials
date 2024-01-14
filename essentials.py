7fe5957791c451156189f38ac75686941e6f1c4b
matt3o <matt3o@gmail.com>
2024-01-14 16:09:28 +0100

    image composite from mask batch

--- essentials.py
+++ essentials.py
@@ -543,6 +543,45 @@ class ImageFromBatch:
         length = min(image.shape[0]-start, length)
         return (image[start:start + length], )
 
+class ImageCompositeFromMaskBatch:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image_from": ("IMAGE", ),
+                "image_to": ("IMAGE", ),
+                "mask": ("MASK", )
+            }
+        }
+    
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "execute"
+    CATEGORY = "essentials"
+
+    def execute(self, image_from, image_to, mask):
+        frames = mask.shape[0]
+
+        if image_from.shape[1] != image_to.shape[1] or image_from.shape[2] != image_to.shape[2]:
+            image_to = p(image_to)
+            image_to = comfy.utils.common_upscale(image_to, image_from.shape[2], image_from.shape[1], upscale_method='bicubic', crop='center')
+            image_to = pb(image_to)
+               
+        if frames < image_from.shape[0]:
+            image_from = image_from[:frames]
+        elif frames > image_from.shape[0]:
+            image_from = torch.cat((image_from, image_from[-1].unsqueeze(0).repeat(frames-image_from.shape[0], 1, 1, 1)), dim=0)
+        
+        mask = mask.unsqueeze(3).repeat(1, 1, 1, 3)
+
+        if image_from.shape[1] != mask.shape[1] or image_from.shape[2] != mask.shape[2]:
+            mask = p(mask)
+            mask = comfy.utils.common_upscale(mask, image_from.shape[2], image_from.shape[1], upscale_method='bicubic', crop='center')
+            mask = pb(mask)
+
+        out = mask * image_to + (1 - mask) * image_from
+
+        return (out, )
+
 class TransitionMask:
     @classmethod
     def INPUT_TYPES(s):
@@ -925,6 +964,7 @@ NODE_CLASS_MAPPINGS = {
     "ImageEnhanceDifference+": ImageEnhanceDifference,
     "ImageExpandBatch+": ImageExpandBatch,
     "ImageFromBatch+": ImageFromBatch,
+    "ImageCompositeFromMaskBatch+": ImageCompositeFromMaskBatch,
     "ExtractKeyframes+": ExtractKeyframes,
 
     "MaskBlur+": MaskBlur,
@@ -959,6 +999,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "ImageEnhanceDifference+": "🔧 Image Enhance Difference",
     "ImageExpandBatch+": "🔧 Image Expand Batch",
     "ImageFromBatch+": "🔧 Image From Batch",
+    "ImageCompositeFromMaskBatch+": "🔧 Image Composite From Mask Batch",
     "ExtractKeyframes+": "🔧 Extract Keyframes (experimental)",
 
     "MaskBlur+": "🔧 Mask Blur",
